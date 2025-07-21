3f20845fa204245b94bc2eb8aad7696a9b57f361
Stiven hoxha <stivenhoxha007@gmail.com>
2025-07-21 16:42:53 +0200

    modifica dell'aspetto per la home page di benvenuto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,9 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-title">Benvenuto nel Comparatore di Frutta! 🍇🍎🍌</h1>
+      <h1 className="home-title">
+        Benvenuto nel Comparatore di Frutta! 🍇🍎🍌
+      </h1>
       <Link to="/fruits" className="home-button">
         Vai alla lista dei frutti →
       </Link>
